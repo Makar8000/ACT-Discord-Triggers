2c77b5b43ea07c71a9215636494a5dc956806542
Marcus Terry <Makartws2@Hotmail.com>
2017-04-19 02:30:40 -0500

    Add basic parsing logic

--- ACT_DiscordTriggers/ACT_DiscordTriggers/MyPlugin.cs
+++ ACT_DiscordTriggers/ACT_DiscordTriggers/MyPlugin.cs
@@ -36,33 +36,75 @@ namespace ACT_Plugin {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.lblLabel = new System.Windows.Forms.Label();
-			this.txtBox = new System.Windows.Forms.TextBox();
+			this.lblBotTok = new System.Windows.Forms.Label();
+			this.txtToken = new System.Windows.Forms.TextBox();
+			this.lblName = new System.Windows.Forms.Label();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.logBox = new System.Windows.Forms.TextBox();
+			this.lblLog = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
-			// lblLabel
+			// lblBotTok
 			// 
-			this.lblLabel.AutoSize = true;
-			this.lblLabel.Location = new System.Drawing.Point(50, 34);
-			this.lblLabel.Name = "lblLabel";
-			this.lblLabel.Size = new System.Drawing.Size(47, 13);
-			this.lblLabel.TabIndex = 0;
-			this.lblLabel.Text = "MyLabel";
+			this.lblBotTok.AutoSize = true;
+			this.lblBotTok.Location = new System.Drawing.Point(28, 23);
+			this.lblBotTok.Name = "lblBotTok";
+			this.lblBotTok.Size = new System.Drawing.Size(96, 13);
+			this.lblBotTok.TabIndex = 0;
+			this.lblBotTok.Text = "Discord Bot Token";
 			// 
-			// txtBox
+			// txtToken
 			// 
-			this.txtBox.Location = new System.Drawing.Point(53, 50);
-			this.txtBox.Name = "txtBox";
-			this.txtBox.Size = new System.Drawing.Size(100, 20);
-			this.txtBox.TabIndex = 1;
-			this.txtBox.Text = "This is a test.";
+			this.txtToken.Location = new System.Drawing.Point(31, 39);
+			this.txtToken.Name = "txtToken";
+			this.txtToken.Size = new System.Drawing.Size(281, 20);
+			this.txtToken.TabIndex = 1;
+			this.txtToken.Text = "Enter your Discord Bot Token";
+			// 
+			// lblName
+			// 
+			this.lblName.AutoSize = true;
+			this.lblName.Location = new System.Drawing.Point(28, 88);
+			this.lblName.Name = "lblName";
+			this.lblName.Size = new System.Drawing.Size(57, 13);
+			this.lblName.TabIndex = 2;
+			this.lblName.Text = "Discord ID";
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(31, 104);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(281, 20);
+			this.txtName.TabIndex = 3;
+			this.txtName.Text = "Enter your Discord ID";
+			// 
+			// logBox
+			// 
+			this.logBox.Location = new System.Drawing.Point(31, 167);
+			this.logBox.Multiline = true;
+			this.logBox.Name = "logBox";
+			this.logBox.Size = new System.Drawing.Size(588, 180);
+			this.logBox.TabIndex = 4;
+			// 
+			// lblLog
+			// 
+			this.lblLog.AutoSize = true;
+			this.lblLog.Location = new System.Drawing.Point(28, 151);
+			this.lblLog.Name = "lblLog";
+			this.lblLog.Size = new System.Drawing.Size(60, 13);
+			this.lblLog.TabIndex = 5;
+			this.lblLog.Text = "Debug Log";
 			// 
 			// MyPlugin
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.txtBox);
-			this.Controls.Add(this.lblLabel);
+			this.Controls.Add(this.lblLog);
+			this.Controls.Add(this.logBox);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.lblName);
+			this.Controls.Add(this.txtToken);
+			this.Controls.Add(this.lblBotTok);
 			this.Name = "MyPlugin";
 			this.Size = new System.Drawing.Size(686, 384);
 			this.Load += new System.EventHandler(this.MyPlugin_Load);
@@ -74,29 +116,46 @@ namespace ACT_Plugin {
 		#endregion
 
 		#endregion
-		public void PluginSample() {
+		public MyPlugin() {
 			InitializeComponent();
 		}
 
 		Label lblStatus;    // The status label that appears in ACT's Plugin tab
 		string settingsFile = Path.Combine(ActGlobals.oFormActMain.AppDataFolder.FullName, "Config\\ACT_DiscordTriggers.config.xml");
-		private Label lblLabel;
-		private TextBox txtBox;
+		private Label lblBotTok;
+		private TextBox txtToken;
+		private Label lblName;
+		private TextBox txtName;
+		private TextBox logBox;
+		private Label lblLog;
 		SettingsSerializer xmlSettings;
 
 		#region IActPluginV1 Members
 		public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText) {
 			lblStatus = pluginStatusText;   // Hand the status label's reference to our local var
 			pluginScreenSpace.Controls.Add(this);   // Add this UserControl to the tab ACT provides
+			pluginScreenSpace.Text = "Discord Triggers";
 			this.Dock = DockStyle.Fill; // Expand the UserControl to fill the tab's client space
 			xmlSettings = new SettingsSerializer(this); // Create a new settings serializer and pass it this instance
-			//LoadSettings();
+			LoadSettings();
 
 			// Create some sort of parsing event handler.  After the "+=" hit TAB twice and the code will be generated for you.
 			ActGlobals.oFormActMain.AfterCombatAction += new CombatActionDelegate(oFormActMain_AfterCombatAction);
+			ActGlobals.oFormActMain.OnLogLineRead += OFormActMain_OnLogLineRead;
 
 			lblStatus.Text = "Plugin Started";
 		}
+
+		private void OFormActMain_OnLogLineRead(bool isImport, LogLineEventArgs logInfo) {
+			foreach (KeyValuePair<string, CustomTrigger> trig in ActGlobals.oFormActMain.CustomTriggers) {
+				if (trig.Value.RegEx.IsMatch(logInfo.logLine)) {
+					logBox.AppendText("Found a match!\n");
+					//this is where I would say something in discord
+					break;
+				}
+			}
+		}
+
 		public void DeInitPlugin() {
 			// Unsubscribe from any events you listen to when exiting!
 			ActGlobals.oFormActMain.AfterCombatAction -= oFormActMain_AfterCombatAction;
@@ -111,7 +170,8 @@ namespace ACT_Plugin {
 		}
 
 		void LoadSettings() {
-			xmlSettings.AddControlSetting(txtBox.Name, txtBox);
+			xmlSettings.AddControlSetting(txtToken.Name, txtToken);
+			xmlSettings.AddControlSetting(txtName.Name, txtName);
 
 			if (File.Exists(settingsFile)) {
 				FileStream fs = new FileStream(settingsFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
@@ -149,7 +209,7 @@ namespace ACT_Plugin {
 		}
 
 		private void MyPlugin_Load(object sender, EventArgs e) {
-
+			
 		}
 	}
 }
