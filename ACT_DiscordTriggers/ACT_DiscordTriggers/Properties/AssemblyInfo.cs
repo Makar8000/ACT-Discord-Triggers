23a9948a781a7c500dc9773e188f3afead264a18
Marcus Terry <Makartws2@Hotmail.com>
2017-04-23 04:09:47 -0500

    Adjust AssemblyInfo

--- ACT_DiscordTriggers/ACT_DiscordTriggers/Properties/AssemblyInfo.cs
+++ ACT_DiscordTriggers/ACT_DiscordTriggers/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ACT_DiscordTriggers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An ACT Plugin for sending triggers through Discord.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ACT_DiscordTriggers")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Makar")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
