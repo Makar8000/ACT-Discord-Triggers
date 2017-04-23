ed819698fbd428acab946b9efb9502fba92c91eb
Marcus Terry <Makartws2@Hotmail.com>
2017-04-23 18:37:37 -0500

    Attempt to fix stuff.

--- ACT_DiscordTriggers/ACT_DiscordTriggers/Properties/AssemblyInfo.cs
+++ ACT_DiscordTriggers/ACT_DiscordTriggers/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ACT_DiscordTriggers")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Makar")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
