73fd455a0404df30ddd10460c9739654adc98b0c
Marcus Terry <Makartws2@Hotmail.com>
2019-04-09 00:53:59 -0500

    Bump version

--- ACT_DiscordTriggers/ACT_DiscordTriggers/Properties/AssemblyInfo.cs
+++ ACT_DiscordTriggers/ACT_DiscordTriggers/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("000fffc0-8d23-4476-b312-0438655973a1")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.1")]
+[assembly: AssemblyFileVersion("1.0.1.1")]
